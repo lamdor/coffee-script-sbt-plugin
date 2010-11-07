6f3dc53a4f8e115aa5dcd796d951835720575355
Nathan Hamblen <nathan@technically.us>
2010-11-07 12:02:35 -0500

    use dynamically generated filetasks to avoid unnecessary coffeescript compiles on unchanged inputs

--- src/main/scala/CoffeeScriptCompile.scala
+++ src/main/scala/CoffeeScriptCompile.scala
@@ -8,7 +8,7 @@ import java.io.File
 trait CoffeeScriptCompile extends BasicScalaProject {
 
   protected def coffeeScriptDirectoryPathFinder = {"src" / "main" / "coffee-script"}
-  protected def coffeeScriptCompiledOuputPath = {
+  protected def coffeeScriptCompiledOutputPath = {
     if (mainArtifact.extension == "war") {
       "src" / "main" / "webapp" / "javascript"
     } else {
@@ -16,34 +16,33 @@ trait CoffeeScriptCompile extends BasicScalaProject {
     }
   }
   protected def coffeeScriptCompiledOuputDirectory =
-    coffeeScriptCompiledOuputPath.asFile.getPath
+    coffeeScriptCompiledOutputPath.asFile.getPath
 
   lazy val coffeeScriptPaths = (coffeeScriptDirectoryPathFinder ##) ** "*.coffee"
 
 
   override def watchPaths = super.watchPaths +++ coffeeScriptPaths
 
-  lazy val compileCoffeeScript = task {
-    log.info("Compiling CoffeeScript")
-
-    val coffeeCompiler = new JCoffeeScriptCompiler()
-
-    coffeeScriptPaths.get.foreach {
-      coffeePath =>
+  lazy val compileCoffeeScript = dynamic(compileCoffeeTasks) describedAs
+    "Compiles CoffeeScript files."
+  
+  lazy private val coffeeCompiler = new JCoffeeScriptCompiler()
+  def compileCoffeeTasks = task { None } dependsOn ({
+    coffeeScriptPaths.get.map { coffeePath =>
       val coffeeFile = coffeePath.asFile
       val jsDestination = new File(coffeeScriptCompiledOuputDirectory,
-                                   "\\.coffee$".r.replaceFirstIn(coffeePath.relativePath, ".js"))
+        "\\.coffee$".r.replaceFirstIn(coffeePath.relativePath, ".js"))
+      log.info(Path.fromFile(jsDestination).toString)
+      log.info(coffeePath.toString)
+      fileTask(Path.fromFile(jsDestination) from coffeePath) {
+        log.info("CoffeeScript compiling %s" format (coffeePath.relativePath))
 
-      log.info("Compiling %s" format (coffeePath.relativePath))
+        val coffeeSource =  Source.fromFile(coffeeFile).mkString
+        val compiledJs = coffeeCompiler.compile(coffeeSource)
 
-      val coffeeSource =  Source.fromFile(coffeeFile).mkString
-      val compiledJs = coffeeCompiler.compile(coffeeSource)
-
-      new File(jsDestination.getParent).mkdirs
-
-      FileUtilities.write(jsDestination, compiledJs, log)
+        new File(jsDestination.getParent).mkdirs()
+        FileUtilities.write(jsDestination, compiledJs, log)
+      }
     }
-
-    None
-  } describedAs("Compiles CoffeeScript files.")
+  }.toSeq: _*)
 }
