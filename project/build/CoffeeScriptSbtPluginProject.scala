dd839ea8ec4e2e2037a8ce468f86618317eab973
Victor Bilyk <victorbilyk@gmail.com>
2011-01-11 14:06:14 +0600

    jcoffeescript dependency now on separate classpath to prevent classpath conflicts

--- project/build/CoffeeScriptSbtPluginProject.scala
+++ project/build/CoffeeScriptSbtPluginProject.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 class CoffeeScriptSbtPluginProject(info: ProjectInfo) extends PluginProject(info) with test.ScalaScripted {
-  val jcoffeescript = "org.jcoffeescript" % "jcoffeescript" % "1.0" from "http://cloud.github.com/downloads/yeungda/jcoffeescript/jcoffeescript-1.0.jar"
+  val jcoffeescript = "org.jcoffeescript" % "jcoffeescript" % "1.0" % "provided" from "http://cloud.github.com/downloads/yeungda/jcoffeescript/jcoffeescript-1.0.jar"
 
   override def scriptedSbt = "0.7.4"
   override def scriptedBufferLog = false
